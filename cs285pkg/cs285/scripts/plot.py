2d1133677a70305a4efa57ce409919447aed2259
Bennyoooo <bennyjiangxinhao@gmail.com>
2020-12-07 00:57:49 -0800

    save progress before merge

---
+++ cs285pkg/cs285/scripts/plot.py
@@ -0,0 +1,50 @@
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../data/')
+PLOT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../plots/')
+
+
+def get_section_results(file):
+    eval_returns = []
+    X = []
+    Z = []
+    for e in tf.train.summary_iterator(file):
+        for v in e.summary.value:
+            if v.tag == 'Eval_AverageReturn':
+                eval_returns.append(v.simple_value)
+            if v.tag == 'Train_EnvstepsSoFar':
+                X.append(v.simple_value)
+            elif v.tag == "Eval_StdReturn":
+                Z.append(v.simple_value)
+
+    return X, eval_returns, np.array(Z)
+
+
+if __name__ == "__main__":
+
+    title = 'comparison'
+    file_names = ['box-close_b3000_box-close-v1_28-10-2020_16-04-17', 'box-close_button-press_b3000_box-close-v1_28-10-2020_17-08-45']
+    labels = ['one_task', 'two_tasks']
+    plt.figure(figsize=(14, 7))
+    for i in range(len(labels)):
+        file_path = DATA_PATH + file_names[i]
+        _, eval_returns, Z = get_section_results(file_path+"/"+os.listdir(file_path)[0])
+        # plot for a single experiment
+        x = range(len(eval_returns))
+
+        plt.plot(x, eval_returns, label=labels[i])
+        plt.fill_between(x, (eval_returns - Z), (eval_returns + Z),  alpha=0.5)
+        plt.xlabel("num of iterations", fontsize=30)
+        plt.ylabel("average evaluation return", fontsize=30)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.title(title)
+    plt.legend(fontsize=30)
+    plt.savefig(PLOT_PATH + title + '.pdf')
+    plt.show()
+
+
+
